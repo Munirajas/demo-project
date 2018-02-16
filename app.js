fdb835037355be0ed295fd233c70a392a77ee2f9
SRIKANTH <srikanth.pavuluri@costrategix.com>
2018-02-17 01:18:23 +0530

    counter timer added

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var app = angular.module('tta',['ngRoute',
 
 app.config(function($routeProvider, $locationProvider, socialProvider) {
   
-  socialProvider.setGoogleKey("977973052600-1ao204k1kc6boqpevtvj81tmia2gipea.apps.googleusercontent.com");
+  //socialProvider.setGoogleKey("977973052600-1ao204k1kc6boqpevtvj81tmia2gipea.apps.googleusercontent.com");
 
   $routeProvider
   .when('/eve', {
@@ -44,7 +44,7 @@ app.config(function($routeProvider, $locationProvider, socialProvider) {
     templateUrl: 'components/presenter/presenter.html',
     controller: 'presenterController'
   })
-  .when('/events', {
+  .when('/', {
     templateUrl: 'components/events/event-list.html',
     controller: 'eventController'
   });
@@ -69,7 +69,44 @@ app.controller('mainController', function($scope,$rootScope,$location){
      localStorage.removeItem('user');
      $location.path('/login');
   }
-
 });
 
+app.directive('countdown', [
+  'Util',
+  '$interval',
+  function (Util, $interval) {
+      return {
+          restrict: 'A',
+          scope: { date: '@' },
+          link: function (scope, element) {
+              var future;
+              future = new Date(scope.date);
+              $interval(function () {
+                  var diff;
+                  diff = Math.floor((future.getTime() - new Date().getTime()) / 1000);
+                  return element.text(Util.dhms(diff));
+              }, 1000);
+          }
+      };
+  }
+]).factory('Util', [function () {
+      return {
+          dhms: function (t) {
+              var days, hours, minutes, seconds;
+              days = Math.floor(t / 86400);
+              t -= days * 86400;
+              hours = Math.floor(t / 3600) % 24;
+              t -= hours * 3600;
+              minutes = Math.floor(t / 60) % 60;
+              t -= minutes * 60;
+              seconds = t % 60;
+              return [
+                  days + 'd',
+                  hours + 'h',
+                  minutes + 'm',
+                  seconds + 's'
+              ].join(' ');
+          }
+      };
+  }]);
 
