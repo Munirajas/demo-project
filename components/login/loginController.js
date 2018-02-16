4e307e96b26ff80d995e00abf811d6b8d35088e8
SRIKANTH <srikanth.pavuluri@costrategix.com>
2018-02-16 20:14:30 +0530

    components added

--- components/login/loginController.js
+++ components/login/loginController.js
@@ -1,5 +1,5 @@
 angular.module('Login')
-  .controller('loginController', function($scope,  $routeParams, loginService) {
+  .controller('loginController', function($scope,  $routeParams, $location, loginService, socialLoginService) {
     var $ctrl = this;	 	
 	
 	var userValues = {};
@@ -7,9 +7,23 @@ angular.module('Login')
     $scope.checkLogin = function () {
     	console.log($scope.user);
     	loginService.validateLogin($scope.user).then(function(_res) {
-           console.log(_res);
+		   console.log(_res);
     	}); 
-    }
+	}
+	
+	$scope.signout = function() {
+    	socialLoginService.logout();
+	}
+
+	$scope.$on('event:social-sign-in-success', (event, userDetails)=> {
+		$scope.result = userDetails;
+		$location.path('/login');
+		$scope.$apply();
+	});
+
+	$scope.$on('event:social-sign-out-success', function(event, userDetails){
+		$scope.result = userDetails;
+	});
 
 });
 
